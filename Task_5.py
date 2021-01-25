b462af7292859e5e41697edfa9b023b763ab3fdc
VDK45 <vdk45blg@gmail.ru>
2021-01-25 20:43:56 +0900

    Пятое задание с float

--- Task_5.py
+++ Task_5.py
@@ -1,38 +1,23 @@
-num = int(input('Введите число: '))
+num = float(input('Введите число: '))
 my = [7, 5, 3, 3, 2]
-x = 0
-while 1:
+for i in my:
     if num > my[0]:
         my.insert(0, num)
         print(my)
         break
-    elif num < my[-1]:
+    elif num <= my[-1]:
         my.append(num)
         print(my)
         break
-    elif num > my[x]:
-        my.insert(x, num)
+    elif num == my[my.index(i)] and num == my[my.index(i) + 1]:
+        my.insert(my.index(i) + 2, num)
         print(my)
         break
-    elif num == my[x]:
-        my.insert(x + 1, num)
+    elif num == my[my.index(i)]:
+        my.insert(my.index(i) + 1, num)
         print(my)
         break
-    x += 1
-
-print("-" * 5, "Второй способ", "-" * 5)
-
-n = int(input('Number enter: '))
-r = [7, 5, 3, 3, 2]
-print('Рейтинг - {}'.format(r))
-for x in range(len(r)):
-    if r[x] == n:
-        r.insert(x + 1, n)
+    elif num < my[my.index(i)] and num > my[my.index(i) + 1]:
+        my.insert(my.index(i) + 1, num)
+        print(my)
         break
-    elif r[0] < n:
-        r.insert(0, n)
-    elif r[-1] > n:
-        r.append(n)
-    elif r[x] > n and r[x + 1] < n:
-        r.insert(x + 1, n)
-print('Получилось - {}'.format(r))
